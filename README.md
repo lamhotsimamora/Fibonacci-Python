ff0ed44e16cbcc284dd8b8fa071390fb1448b6bd
lamhotsimamora <lamhotsimamora36@gmail.com>
2018-02-08 21:43:00 +0700

    add project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Fibonacci-Python
+
+
+v 3.6.2 Python
+
+
+##  Executable
+dist/fibonacci.exe
+
+##  Source
+src/fibonacci.py
---
+++ src/fibonacci.py
@@ -0,0 +1,108 @@
+# @lamhotsimamora
+# Fibonacci Application
+# 2018
+import os
+
+class Fibonacci: 
+    def __init__(self,_length):
+        self._length = _length
+
+    def newFibonacci(self):
+        start = 0
+        data = []
+        for i in range(self._length):
+        	if i==0:
+        		data.append(start)
+        		start = start + 1 
+        	else:
+        		data.append(start)
+        		start = data[i] + data[i-1]
+        return data 
+
+def resultFibo(a):
+    fibo = Fibonacci(a)
+    result = fibo.newFibonacci()  
+    print(str(result))
+    showMenu()
+
+def processFibo(num):
+    length = 120
+    fibo = Fibonacci(length)
+    res  = fibo.newFibonacci()
+    if num in res:
+        print('Number '+str(num)+' is fibonacci')
+    else:
+        print('Number '+str(num)+' is not fibonacci')
+    showMenu()
+
+def createFibo():
+    os.system('cls')
+    check = True
+    while check:
+        val = input('How many number of fibonacci do you want ? ')
+        try:
+            get   = int(val)
+            check = False
+            resultFibo(get)
+            break
+        except ValueError:
+            check = True
+            createFibo() 
+
+def checkFibo():
+    os.system('cls')
+    check = True
+    print(" ")
+    while check:
+        val = input('Input the number ! ')
+        try:
+            get   = int(val)
+            check = False
+            processFibo(get)
+            break
+        except ValueError:
+            check = True
+            createFibo() 
+   
+menu = None
+def showMenu():
+    check = True
+    print(" ")
+    while check:
+        menu = input("Select Menu : ")
+        try:
+            get   = int(menu)
+            check = False
+            if get ==  1:
+               createFibo()
+               break
+            elif get == 2: 
+               checkFibo()
+               break
+            elif get == 3:
+                about()
+                break
+            else:
+               showMenu()
+        except ValueError:
+            check = True
+            showMenu() 
+
+def about():
+    print(" ")
+    print('Developer @lamhotsimamora')
+    print('Fibonacci Application v.1.0')
+    print('https://github.com/lamhotsimamora')
+    showMenu()
+
+print("*********************************************")
+print("================ SELECT MENU ================")
+print("*********************************************")
+print("1.  Generate Fibonacci                       ")
+print("2.  Check Fibonacci Number                   ")
+print("3.  About                                    ")
+print("*********************************************")
+showMenu()
+
+
+
